d5d31236ff511d39fd86d656f0c260ae810bd331
Yang <Yang@59.12.7.231>
2019-03-25 23:42:29 +0900

    add 2839

---
+++ back/src/back/back2839.java
@@ -0,0 +1,41 @@
+package back;
+
+import java.util.Scanner;
+
+public class back2839 {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		int count5 = 0;
+		int count3 =0;
+		
+		
+		while(true) { 
+			if(input%5!=0 &&input>=0) {
+				input -=3;
+				count3++;
+		
+			}else {
+				break;
+			}
+		}
+		if(input<0) {
+			System.out.println(-1);
+		}
+		else {
+			count5=input/5;
+			System.out.println(count3+count5);
+		}
+		
+			
+		
+			
+			
+			
+		
+	
+
+	}
+
+}
