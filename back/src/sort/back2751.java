7da9e78582760565db35a99216346a3e3eedf734
yangseungin <rhfpdk92@naver.com>
2019-03-29 17:52:16 +0900

    timed out...

--- back/src/sort/back2751.java
+++ back/src/sort/back2751.java
@@ -1,28 +1,32 @@
 package sort;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class back2751 {
 	static int result[];
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		Scanner sc = new Scanner(System.in);
-		 int count = sc.nextInt();
+		
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	     StringBuilder sb = new StringBuilder();
+	     int count= Integer.parseInt(br.readLine().trim());
 		 int[] Arr = new int[count];
 		 result = new int[count];
 
 		 for(int i=0;i<count;i++){
-			 Arr[i]=sc.nextInt();
+			 Arr[i]=Integer.parseInt(br.readLine().trim());
 		 }
-		
-
 		
 
 		merge_sort(Arr,0,count-1);
 		
-
-		printArr(result);
+		for(int i=0;i<count;i++){
+			sb.append(result[i]+"\n");
+		}
+		System.out.println(sb);
 
 	}
 	public static void merge_sort(int[] arr, int l,int r) {    
@@ -52,74 +56,4 @@ public class back2751 {
         for(int i=left;i<right+1;i++)    
             arr[i] = result[i];
     }
-	
-	
-
-	public static void printArr(int[] Arr) {
-		for (int i = 0; i < Arr.length; i++) {
-			System.out.println(Arr[i]);
-		}
-
-	}
-
-	public static int[] divideArr_not_use(int[] arr) {
-		int[] temp = null;
-		int[] subArr1 = null;
-		int[] subArr2 = null;
-		if(arr.length!=1){
-			
-		}else{
-			return arr;
-		}
-
-		/*
-		 * asdf if( arr size!=1) //arr에 원소 두개이상이면 ~~~~
-		 * 
-		 * 좌측 재귀 arr1 
-		 * 
-		 *  
-		 *  우측 재귀
-		 * arr2 
-		 * 
-		 * 
-		 * 
-		 * else //원소가 1개면 return array 원소한개반환
-		 * 
-		 * return combine(arr1,arr2);
-		 */
-
-		return merge_sort_not_use(subArr1,subArr2);
-
-	}
-
-	public static int[] merge_sort_not_use(int[] arr1, int[] arr2) {
-		int arr1Size = arr1.length;
-		int arr2Size = arr2.length;
-
-		int[] temp = new int[arr1Size + arr2Size];
-		int arr1Num = 0, arr2Num = 0;
-		for (int i = 0; i < arr1Size + arr2Size; i++) {
-			System.out.println(arr1Num + " /" + arr2Num);
-			if (arr1Num == arr1Size) {
-				temp[i] = arr2[arr2Num];
-				arr2Num++;
-				continue;
-			} else if (arr2Num == arr2Size) {
-				temp[i] = arr1[arr1Num];
-				arr1Num++;
-				continue;
-			}
-
-			if (arr1[arr1Num] < arr2[arr2Num]) {
-				temp[i] = arr1[arr1Num];
-				arr1Num++;
-			} else {
-				temp[i] = arr2[arr2Num];
-				arr2Num++;
-			}
-		}
-
-		return temp;
-	}
-
 }
