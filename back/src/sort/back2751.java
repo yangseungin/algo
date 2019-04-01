40b621248f0e7a24d6f0da9c348da0ace8a4070c
yangseungin <rhfpdk92@naver.com>
2019-04-01 21:46:49 +0900

    solved 2751

--- back/src/sort/back2751.java
+++ back/src/sort/back2751.java
@@ -1,57 +1,71 @@
 package sort;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
+import java.util.Scanner;
+ 
 public class back2751 {
-	static int result[];
-	
-	public static void main(String[] args) throws IOException {		
-		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	     StringBuilder sb = new StringBuilder();
-	     int count= Integer.parseInt(br.readLine().trim());
-		 int[] Arr = new int[count];
-		 result = new int[count];
-
-		 for(int i=0;i<count;i++){
-			 Arr[i]=Integer.parseInt(br.readLine().trim());
-		 }
-		
-
-		merge_sort(Arr,0,count-1);
-		
-		for(int i=0;i<count;i++){
-			sb.append(result[i]+"\n");
-		}
-		System.out.println(sb);
-
-	}
-	public static void merge_sort(int[] arr, int l,int r) {    
-        int m=0;
-        if(l<r) {
-            m = (l+r)/2;
-            merge_sort(arr,l,m);    
-            merge_sort(arr,m+1,r);
-            merge(arr,l,m,r);    //재귀호출다끝나서 다쪼개지면 합치기
+    static int[] result;
+ 
+    public static void mergeSort(int left, int right, int[] arr) {
+        int mid;
+        if (left < right) {
+            mid = (left + right) / 2;
+            mergeSort(left, mid, arr);
+            mergeSort(mid + 1, right, arr);
+            merge(left, right, mid, arr);
         }
     }
-    public static void merge(int[] arr, int left,int mid,int right) {
-        int l = left;    
-        int m = mid+1;    
-        int k = left;    
-        
-        while(l<=mid || m<=right) {   
-            if(l<=mid && m<=right) {   
-                if(arr[l]<=arr[m]) result[k] = arr[l++];    
-                else result[k] = arr[m++];
+ 
+    public static void merge(int left, int right, int mid, int[] arr) {
+        int l = left;
+        int m = mid + 1;
+        int k = left;
+ 
+        while(l <= mid || m <= right) {
+            if(l <= mid && m <= right) {
+                if(arr[l] <= arr[m]) {
+                    result[k] = arr[l];
+                    l++;
+                }
+                else {
+                    result[k] = arr[m];
+                    m++;
+                }
             }
-            else if(l<=mid && m > right)     
-                result[k] = arr[l++];
-            else result[k] = arr[m++];        
+ 
+            else if(l <= mid && m > right) {
+                result[k] = arr[l];
+                l++;
+            }
+            else {
+                result[k] = arr[m];
+                m++;
+            }
             k++;
         }
-        for(int i=left;i<right+1;i++)    
+ 
+        for(int i = left; i <= right; i++) {
             arr[i] = result[i];
+        }
+ 
+ 
+    }
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        result = new int[n];
+        int[] arr = new int[n];
+ 
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+ 
+        mergeSort(0, n-1, arr);
+ 
+        for(int i=0;i<n;i++) {
+            System.out.println(arr[i]);
+        }
+ 
+        sc.close();
     }
 }
