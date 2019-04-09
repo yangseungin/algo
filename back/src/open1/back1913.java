4ce1c8aed57f3c34bd0ce165eb1fe7d8d70c62f9
yangseungin <rhfpdk92@naver.com>
2019-04-09 10:38:16 +0900

    달팽이배열

--- back/src/open1/back1913.java
+++ back/src/open1/back1913.java
@@ -8,13 +8,41 @@ public class back1913 {
 		Scanner sc = new Scanner(System.in);
 
 		int n = sc.nextInt();
+		int where = sc.nextInt();// 찾을 값
+		String location = "";// 출력위치
+		int value = n * n;
+		int row = -1, col = 0;
+		int index = 1;// 증감량 1or -1
 
 		int[][] arr = new int[n][n];
-		
-		for (int i = 0; i < n; i++) {
 
-			
+		while (n > 0) {
+			for (int i = 0; i < n; i++) {
+				row += index;
+				arr[row][col] = value;
+				if (value == where)
+					location = (row + 1) + " " + (col + 1);
+				value--;
+			}
+			n--;
+			for (int i = 0; i < n; i++) {
+				col += index;
+				arr[row][col] = value;
+				if (value == where)
+					location = (row + 1) + " " + (col + 1);
+				value--;
+			}
+			index *= -1;
+
 		}
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println(location);
 
 	}
 
---
+++ back/src/open1/back1934.java
@@ -0,0 +1,42 @@
+package open1;
+
+import java.util.Scanner;
+
+public class back1934 {
+	public static void main(String[] args) {
+
+		 //유클리드 호제법으로 최대공약수 구한후 최대공약수로 쭉 나눈값과 최대공약수를 곱해 최소공배수를 구함
+		Scanner sc = new Scanner(System.in);
+		int n =sc.nextInt();
+		int[] result= new int[n];
+		
+		for(int i=0;i<n;i++){
+			int a=sc.nextInt();
+			int b = sc.nextInt();
+			int tmp=0;
+			if(a<b){
+				tmp=a;
+				a=b;
+				b=tmp;
+			}			
+			int gcd=gcd(a,b);
+			
+			a/=gcd;
+			b/=gcd;
+			result[i]=a*b*gcd;
+			
+			
+		}
+		for(int i=0;i<n;i++){
+			System.out.println(result[i]);
+		}
+		
+	}
+
+	public static int gcd(int p, int q) {
+		if (q == 0)
+			return p;
+		return gcd(q, p % q);
+	}
+
+}
