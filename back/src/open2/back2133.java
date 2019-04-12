5d71e4bf84672c1ea231d1690d1e5cfe243f7c2b
yangseungin <rhfpdk92@naver.com>
2019-04-12 14:01:26 +0900

    미완

--- back/src/open2/back2133.java
+++ back/src/open2/back2133.java
@@ -2,27 +2,22 @@ package open2;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class back2133 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		//아마 짝수여야지만 채울수 있는것으로 판단됨
-		//2일떄 3개
-		//4일떄 11개
-		//6일때  41개 (3*2)*(3*4)+(3*2)(3*4)문양+3*6고유문양 2
-		// (n-2)*(n-4)+(n-2)*(n-4고유)*2
-		//
 		
-		//n-2 x3
-		//n-4 x2
-		//n-6 x2
+		/* 2	3
+		 * 4	3*2 x 3*2 + 2고유
+		   6	3*2 x 3*4 + 3*2 고유 x 3*2 + 3*6고유 
+		*/
 		
-		//(n-?)이값이 0이되지않는때까지 x2
-		//를 돌면됨
+		int n= Integer.parseInt(br.readLine());
 		
 		
 		
