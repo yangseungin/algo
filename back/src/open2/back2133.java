57b05b36032a609b33e8e16caeefe2aa1f776f4e
yangseungin <rhfpdk92@naver.com>
2019-04-13 21:06:16 +0900

    3xn tiling

--- back/src/open2/back2133.java
+++ back/src/open2/back2133.java
@@ -7,22 +7,43 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class back2133 {
+
+	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));		
 		
 		/* 2	3
 		 * 4	3*2 x 3*2 + 2고유
-		   6	3*2 x 3*4 + 3*2 고유 x 3*2 + 3*6고유 
+		   6	3*2 x 3*4 + 3*4 고유 x 3*2 + 3*6고유 
+		   8	3*2 x 3*6 + 3*4 고유 x 3*4 + 3*6고유 x 3*2 +3*6고유
 		*/
 		
-		int n= Integer.parseInt(br.readLine());
+		int n= Integer.parseInt(br.readLine());		
+		bw.write(String.valueOf(tiling(n)));
+		bw.flush();
 		
 		
+	}
+	public static int tiling(int n) {
+		int answer=0;
 		
+		if(n%2==1) {
+			return 0;
+		}
+		else if(n==0) {
+			return 1;
+		}
+		else if(n==2) {
+			return 3; //3*2 는 3 고정
+		}		
 		
-		
+		answer = 3*tiling(n-2);// 첫케이스 그다음부턴 반복으로 고유 더하기
+		for(int i=0;i<=n-4;i++) {
+			answer+= 2*tiling(i);
+		}
+				
+		return answer;
 	}
 
 }
