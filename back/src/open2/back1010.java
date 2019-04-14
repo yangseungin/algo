163df013d513d8f0f49fcbf13360a707965f48da
yangseungin <rhfpdk92@naver.com>
2019-04-14 16:52:17 +0900

    다리놓기인데 커밋 잘못함

--- back/src/open2/back1010.java
+++ back/src/open2/back1010.java
@@ -9,7 +9,7 @@ import java.io.OutputStreamWriter;
 public class back1010 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		/*
-		 *	서쪽에 N개의 사이트(사이트는 다리지을수있는 점)
+		 *	서쪽에 N개의 사이트(사이트는 다리지을수있는 점) 
 		 *	동쪽에M개( N<M)
 		 *	한개의점은 한개의다리만 가능
 		 * 
