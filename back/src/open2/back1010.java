72cd07d7327b2a1efec13ead7a18a505d5a4cce7
yangseungin <rhfpdk92@naver.com>
2019-04-14 16:50:48 +0900

    야근지수 효율성실패...

---
+++ back/src/open2/back1010.java
@@ -0,0 +1,41 @@
+package open2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class back1010 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		/*
+		 *	서쪽에 N개의 사이트(사이트는 다리지을수있는 점)
+		 *	동쪽에M개( N<M)
+		 *	한개의점은 한개의다리만 가능
+		 * 
+		 * 
+		 */
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N=Integer.parseInt(br.readLine());
+		int M=Integer.parseInt(br.readLine());
+		int result=0;
+		
+		for(int i=0;i<N;i++) {
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
