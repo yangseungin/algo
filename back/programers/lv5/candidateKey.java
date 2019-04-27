7714fd3743417f10a5fd7101ffb95e927b52bb4a
yangseungin <rhfpdk92@naver.com>
2019-04-28 00:11:14 +0900

    후보키 음...

---
+++ back/programers/lv5/candidateKey.java
@@ -0,0 +1,45 @@
+package lv5;
+
+public class candidateKey {
+	public static void main(String[] args) {
+		
+		/*
+		 * 유일성과 최소성을 맞춰줘야한다.. 
+		 * 모든조합을 구해서 각 조합이 유일성과 최소성을 만족하는지 구
+		 * 음 일단 유일성 만족하는 리스트만들고 거기서 최소성 해당안하는애들 빼주면 될거같
+		 * 학번 이름 전공 학년으로 가능한 가지수 15개
+		 * 이중에 
+		
+		
+		*/
+		
+		
+		
+		
+		
+		
+		String[][] relation={{"100","ryan","music","2"},{"200","apeach","math","2"},{"300","tube","computer","3"},
+				{"400","con","computer","4"},{"500","muzi","music","3"},{"600","apeach","music","2"}};
+		
+		System.out.println(solution(relation));
+		
+		
+		
+	}
+	public static int solution(String[][] relation){
+		int answer=0;
+		int rowSize= relation.length;
+		int colSize= relation[0].length;
+		
+		
+		
+		
+		
+		
+		return answer;
+	}
+	
+
+
+
+}
