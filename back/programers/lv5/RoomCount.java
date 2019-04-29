137d698aee476d0eee4ca348bbfa91c0920d2112
yangseungin <rhfpdk92@naver.com>
2019-04-29 19:08:24 +0900

    방갯수구하기

---
+++ back/programers/lv5/RoomCount.java
@@ -0,0 +1,20 @@
+package lv5;
+
+public class RoomCount {
+	public static void main(String[] args) {
+		int[] arrows = {6, 6, 6, 4, 4, 4, 2, 2, 2, 0, 0, 0, 1, 6, 5, 5, 3, 6, 0};
+		
+		System.out.println(solution(arrows));
+		
+		
+	}	
+	
+	public static int solution(int[] arrows){
+		int answer=0;
+		
+		
+		
+		return answer;
+	}
+
+}
