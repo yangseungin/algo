de70cf5a13b9f574e9e83677d160b0d1d5d5729b
yangseungin <rhfpdk92@naver.com>
2019-04-30 22:29:23 +0900

    방의갯수 방문횟수로 구해보기

--- back/programers/lv5/RoomCount.java
+++ back/programers/lv5/RoomCount.java
@@ -2,6 +2,14 @@ package lv5;
 
 public class RoomCount {
 	public static void main(String[] args) {
+		
+	/*	그래프 음....
+	 *	뭔가 돌다 다시 자기자리에왔을떄체크를 하면 될거 같은데 음..
+	 * 	
+		
+		*/
+		
+		
 		int[] arrows = {6, 6, 6, 4, 4, 4, 2, 2, 2, 0, 0, 0, 1, 6, 5, 5, 3, 6, 0};
 		
 		System.out.println(solution(arrows));
