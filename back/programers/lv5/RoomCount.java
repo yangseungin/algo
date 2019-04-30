d48f0373df6e964d43713714176d96cfc824bb16
yangseungin <rhfpdk92@naver.com>
2019-04-30 22:49:39 +0900

    방의개수 구하는방법 찾은거같음

--- back/programers/lv5/RoomCount.java
+++ back/programers/lv5/RoomCount.java
@@ -1,10 +1,15 @@
 package lv5;
 
 public class RoomCount {
+	static int numX=0;
+	static int numY=0;
+	
 	public static void main(String[] args) {
 		
 	/*	그래프 음....
 	 *	뭔가 돌다 다시 자기자리에왔을떄체크를 하면 될거 같은데 음..
+	 	한번 간적있던 점에 방문하면 도형이 하나씩 추가됨 그러나 이동으로인해 그어진선이 기존에 있떤 선이면 안됨  오오.. 가능할삘인
+	 	점과점사이를 쪼개어 반토막씩 내고 그가운데점이 체크되면 지나온선으로 해서 구하면 될거 같음 굿
 	 * 	
 		
 		*/
@@ -19,10 +24,42 @@ public class RoomCount {
 	
 	public static int solution(int[] arrows){
 		int answer=0;
+		for(int i=0;i<arrows.length;i++){
+			move(arrows[i]);
+		}
+		
+		
+		System.out.println("x,y : "+numX+" , "+numY);
+		
 		
 		
 		
 		return answer;
+	}
+	public static void move(int arrow){
+		if(arrow==0){
+			numY++;
+		}else if(arrow==1){
+			numX++;
+			numY++;
+		}else if(arrow==2){
+			numX++;
+		}else if(arrow==3){
+			numX++;
+			numY--;
+		}else if(arrow==4){
+			numY--;
+		}else if(arrow==5){
+			numX--;
+			numY--;
+		}else if(arrow==6){
+			numX--;
+		}else if(arrow==7){
+			numX--;
+			numY++;
+		}
+		
+		
 	}
 
 }
