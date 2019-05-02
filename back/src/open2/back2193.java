de1360694c2bbd300900d1b5cd9a606182d1b649
yangseungin <rhfpdk92@naver.com>
2019-05-02 09:13:10 +0900

    이친수 완료

--- back/src/open2/back2193.java
+++ back/src/open2/back2193.java
@@ -16,20 +16,24 @@ public class back2193 {
 		 * 5	10000 10001 10010 10100 10101
 		 * 6	100000 100001 100010 100100 100101 101000 101001 101010
 		 * 7	1000000 1000001 1000010 1000100 1000101 1001000 1001001 1001010 1010000 1010001 1010010 1010100 1010101
+		 * 
+		 * 		규칙 찾아서 풀었는데 자꾸 시작과동시에 실패떠서봤더니 int 범위 넘어서서 그런
+
 		 * */
+		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		int n=Integer.parseInt(br.readLine()); 
 		
-		int[][] arr = new int[n][];
+		long[] arr = new long[n+2];
 		
-		arr[0][0]=0;
-		arr[1][0]=1;
-		for(int i=2;i<n;i++){
-			
-			
-			 
+		arr[0]=0;
+		arr[1]=1;
+		
+		for(int i=2;i<=n;i++){
+			arr[i]=arr[i-1]+arr[i-2]; 
 			
 		}
 		
@@ -38,7 +42,7 @@ public class back2193 {
 		
 		
 		
-		bw.write(String.valueOf(n));
+		bw.write(String.valueOf(arr[n]));
 		bw.flush();
 		
 	}
