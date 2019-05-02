46ac89180d1b8156a10927c71a8c3ed41d523b57
yangseungin <rhfpdk92@naver.com>
2019-05-02 09:24:17 +0900

    동전2 미완

---
+++ back/src/open2/back2294.java
@@ -0,0 +1,20 @@
+package open2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class back2294 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n=Integer.parseInt(br.readLine());
+		
+		bw.write(String.valueOf(n));;
+		bw.flush();
+	}
+
+}
