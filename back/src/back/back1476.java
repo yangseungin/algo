8fdd4d45d32db4d13ac20836fe94d0cae9f291da
yangseungin <rhfpdk92@naver.com>
2019-05-05 17:49:54 +0900

    1476날짜계산

---
+++ back/src/back/back1476.java
@@ -0,0 +1,23 @@
+package back;
+
+import java.util.Scanner;
+
+public class back1476 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int year=1;
+		
+		int E=sc.nextInt();
+		int S=sc.nextInt();
+		int M=sc.nextInt();
+		while(true){
+			if((year-E)%15==0&&(year-S)%28==0&&(year-M)%19==0){
+				System.out.println(year);
+				break;
+			}
+			year++;
+		}
+	}
+
+}
