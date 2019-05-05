0bc5bd98650668a8d0892102534cf64ad1be1484
yangseungin <rhfpdk92@naver.com>
2019-05-05 18:01:43 +0900

    11365 일급비밀

---
+++ back/src/back/back11365.java
@@ -0,0 +1,35 @@
+package back;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class back11365 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String str="";
+
+		while(true){
+			str=br.readLine();
+			if(str.equals("END"))
+				break;
+			else{
+				for(int i=str.length()-1;i>=0;i--){
+					bw.write(str.charAt(i));
+					
+				}bw.newLine();
+			}
+		}
+		
+		
+
+		bw.flush();
+		
+	
+	}
+
+}
