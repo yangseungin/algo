4a63ee35d86dbfbfb62de5d5f245676e1054be09
yangseungin <rhfpdk92@naver.com>
2019-05-06 23:29:00 +0900

    지형편집 미완

---
+++ back/programers/lv4/EditTerrain.java
@@ -0,0 +1,21 @@
+package lv4;
+
+public class EditTerrain {
+	public static void main(String[] args) {
+		// 지형 편집
+		// https://programmers.co.kr/learn/courses/30/lessons/12984
+
+		int[][] land = { { 4, 4, 3 }, { 3, 2, 2 }, { 2, 1, 0 } };
+		int P = 5;
+		int Q = 3;
+
+		System.out.println(solution(land, P, Q));
+
+	}
+
+	public static long solution(int[][] land, int P, int Q) {
+		long answer = -1;
+
+		return answer;
+	}
+}
