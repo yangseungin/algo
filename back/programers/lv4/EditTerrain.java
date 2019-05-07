af9462c5a60561f19277b8ee03cd11987ac00fbd
yangseungin <rhfpdk92@naver.com>
2019-05-07 09:09:48 +0900

    지형편집 미완

--- back/programers/lv4/EditTerrain.java
+++ back/programers/lv4/EditTerrain.java
@@ -1,6 +1,9 @@
 package lv4;
 
 public class EditTerrain {
+
+	static int size;
+
 	public static void main(String[] args) {
 		// 지형 편집
 		// https://programmers.co.kr/learn/courses/30/lessons/12984
@@ -13,8 +16,39 @@ public class EditTerrain {
 
 	}
 
+	public static long height(int[][] land, int middle, int P, int Q) {
+		long value = 0;
+
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+			
+
+			}
+		}
+
+		return value;
+	}
+
 	public static long solution(int[][] land, int P, int Q) {
+		size = land.length;
 		long answer = -1;
+
+		int start = 0;
+		int end = 300;
+		int middle = 0;
+
+		// 최저점을 찾아야하는데
+		while (start <= end) {
+			middle = (start + end) / 2;
+			long[] arr = { height(land, middle, P, Q), height(land, middle + 1, P, Q) };
+
+			if (arr[0] < arr[1]) {
+				end = middle - 1;
+			} else {
+				start = middle + 1;
+			}
+
+		}
 
 		return answer;
 	}
