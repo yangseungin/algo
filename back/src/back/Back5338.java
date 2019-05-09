103101a7db8fc2ae45da53f525c07bb768313f97
yangseungin <rhfpdk92@naver.com>
2019-05-10 08:42:49 +0900

    잡다함

---
+++ back/src/back/Back5338.java
@@ -0,0 +1,12 @@
+package back;
+
+public class Back5338 {
+	public static void main(String[] args) {
+		System.out.println("       _.-;;-._");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-;;-._|");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-''-._|");
+	}
+
+}
