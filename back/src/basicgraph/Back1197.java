b320f070efa1dfc03d35b7f3bb53df282b1fd079
yangseungin <rhfpdk92@naver.com>
2019-05-10 17:54:07 +0900

    1197그래프 아직안했음..

---
+++ back/src/basicgraph/Back1197.java
@@ -0,0 +1,20 @@
+package basicgraph;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Back1197 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int V=Integer.parseInt(st.nextToken());
+		int E= Integer.parseInt(st.nextToken());
+	}
+
+}
