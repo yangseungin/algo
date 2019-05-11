76cb83f503ea2f9bc8f86cb2f32b3d0de33be28b
yangseungin <rhfpdk92@naver.com>
2019-05-11 23:05:05 +0900

    저울 시작

---
+++ back/programers/lv3/Scale.java
@@ -0,0 +1,14 @@
+package lv3;
+
+public class Scale {
+	public static void main(String[] args) {
+		int[] weight={3, 1, 6, 2, 7, 30, 1};
+		System.out.println(solution(weight));
+	}
+	public static int solution(int[] weight){
+		int answer=0;
+		
+		return answer;
+	}
+
+}
