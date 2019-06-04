44949cb569df68d533069d51743b061b285fd9a2
seungin yang <rhfpdk92@naver.com>
2019-06-04 23:48:53 +0900

    전화번호목록

--- back/programers/highscorekit/hash/NumberList.java
+++ back/programers/highscorekit/hash/NumberList.java
@@ -2,10 +2,19 @@ package highscorekit.hash;
 
 public class NumberList {
 	public static void main(String[] args) {
+		System.out.println(solution(new String[]{"119","95375239","119535"}));
+		System.out.println(solution(new String[]{"123","456","789"}));
 		
 	}
 	public static boolean solution(String[] phone_book) {
         boolean answer = true;
+        
+        for(int i=0;i<phone_book.length;i++){
+        	for(int j=i+1;j<phone_book.length;j++){
+        		if(phone_book[j].startsWith(phone_book[i]))
+        			return false;
+        	}
+        }
         return answer;
     }
 
