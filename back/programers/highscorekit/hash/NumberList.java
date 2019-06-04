0a410bb305cf14503196140097556cd2b0a145d7
seungin yang <rhfpdk92@naver.com>
2019-06-05 00:07:54 +0900

    잘못올림

--- back/programers/highscorekit/hash/NumberList.java
+++ back/programers/highscorekit/hash/NumberList.java
@@ -11,7 +11,7 @@ public class NumberList {
         
         for(int i=0;i<phone_book.length;i++){
         	for(int j=i+1;j<phone_book.length;j++){
-        		if(phone_book[j].startsWith(phone_book[i]))
+        		if(phone_book[j].startsWith(phone_book[i])||phone_book[i].startsWith(phone_book[j]))
         			return false;
         	}
         }
