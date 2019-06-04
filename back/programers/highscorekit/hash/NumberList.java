6ad0568555a6adecb34f17f7dd788279012550b7
seungin yang <rhfpdk92@naver.com>
2019-06-04 17:26:48 +0900

    ㅇ

---
+++ back/programers/highscorekit/hash/NumberList.java
@@ -0,0 +1,12 @@
+package highscorekit.hash;
+
+public class NumberList {
+	public static void main(String[] args) {
+		
+	}
+	public static boolean solution(String[] phone_book) {
+        boolean answer = true;
+        return answer;
+    }
+
+}
