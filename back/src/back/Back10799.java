f308f9dc923456d703e6b4be0cb9a9e1dc359d5d
seungin yang <rhfpdk92@naver.com>
2019-06-12 08:53:31 +0900

    쇠막대기

---
+++ back/src/back/Back10799.java
@@ -0,0 +1,31 @@
+package back;
+
+import java.util.Scanner;
+public class Back10799 {
+	public static void main(String[] args) {
+		
+        Scanner sc = new Scanner(System.in);
+		System.out.println(solution(sc.nextLine()));
+		
+		
+		
+		
+	}
+	public static int solution(String arrangement){
+		int answer=0;
+		arrangement=arrangement.replace("()", "|");
+		int count=0;
+		for(int i=0;i<arrangement.length();i++){
+			if(arrangement.charAt(i)=='(')
+				count++;
+			else if(arrangement.charAt(i)==')'){
+				count--;
+				answer++;
+			}else
+				answer+=count;	
+		}
+		return answer;		
+	}
+
+}
+
