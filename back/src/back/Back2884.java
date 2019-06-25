3ea12c21832c59b3a5c1a717ef10949b92fad8ec
seungin yang <rhfpdk92@naver.com>
2019-06-25 22:41:46 +0900

    2884 알람시계

---
+++ back/src/back/Back2884.java
@@ -0,0 +1,25 @@
+package back;
+
+import java.util.Scanner;
+
+public class Back2884 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int hh=sc.nextInt();
+		int mm=sc.nextInt();
+		
+		if(mm>45)
+			mm-=45;
+		else{
+			if(hh-1<0)
+				hh=23;
+			else
+				hh-=1;
+			mm=60+mm-45;
+			
+		}
+		System.out.println(hh +" "+mm);
+	}
+
+}
