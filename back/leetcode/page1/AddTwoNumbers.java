4b4a6630ca2063e13958ddcfdf52c09eb02ff739
seungin yang <rhfpdk92@naver.com>
2019-08-13 08:31:01 +0900

    AddTwoNumbers

---
+++ back/leetcode/page1/AddTwoNumbers.java
@@ -0,0 +1,93 @@
+package page1;
+
+public class AddTwoNumbers {
+	public static void main(String[] args) {
+		ListNode l1 = new ListNode(2);
+		l1.next = new ListNode(4);
+
+		ListNode l2 = new ListNode(5);
+		l2.next = new ListNode(6);
+		l2.next.next = new ListNode(4);
+		ListNode result = addTwoNumbers(l1, l2);
+
+		while (result != null) {
+			System.out.println(result.val);
+			result = result.next;
+		}
+	}
+
+	public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		ListNode resultNode = null;
+		ListNode nextNode = null;
+		int carry = 0;
+		while (l1 != null && l2 != null) {
+			int digitSum = l1.val + l2.val + carry;
+			l1 = l1.next;
+			l2 = l2.next;
+			carry=cal(digitSum);
+			digitSum %= 10;
+
+			if (resultNode == null) {
+				resultNode = new ListNode(digitSum);
+				nextNode = resultNode;
+			} else {
+				nextNode.next = new ListNode(digitSum);
+				nextNode = nextNode.next;
+			}
+
+			if (l1 == null && l2 == null && carry == 1) {
+				nextNode.next = new ListNode(carry);
+				nextNode = nextNode.next;
+			}
+		}
+		while (l1 != null) {
+			int digitSum = l1.val + carry;
+			carry=cal(digitSum);
+			digitSum %= 10;
+			nextNode.next = new ListNode(digitSum);
+			nextNode = nextNode.next;
+			l1 = l1.next;
+			
+			if (l1 == null && carry == 1) {
+				nextNode.next = new ListNode(carry);
+				nextNode = nextNode.next;
+			}
+		}
+		while (l2 != null) {
+			int digitSum = l2.val + carry;
+			carry=cal(digitSum);
+			digitSum %= 10;
+			nextNode.next = new ListNode(digitSum);
+			nextNode = nextNode.next;
+			l2 = l2.next;
+			
+			if (l2 == null && carry == 1) {
+				nextNode.next = new ListNode(carry);
+				nextNode = nextNode.next;
+			}
+		}
+
+		return resultNode;
+	}
+	public static int cal(int digitSum){
+		int num=0;
+		if (digitSum >= 10) {
+			num = 1;
+			digitSum %= 10;
+		} else {
+			num = 0;
+		}
+		
+		return num;
+	}
+
+	public static class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+		}
+	}
+
+}
