2531dd7dc9db98098a1409272ac81f0fe99559ee
seungin yang <rhfpdk92@naver.com>
2019-08-22 08:46:50 +0900

    integer-to-roman

---
+++ back/leetcode/page1/IntegerToRoman.java
@@ -0,0 +1,80 @@
+package page1;
+
+import java.util.HashMap;
+
+public class IntegerToRoman {
+	public static void main(String[] args) {
+		System.out.println(intToRoman(1994));
+
+	}
+
+	public static String intToRoman(int num) {
+		HashMap<Integer, Character> hashMap = new HashMap<>();
+		hashMap.put(1, 'I');
+		hashMap.put(5, 'V');
+		hashMap.put(10, 'X');
+		hashMap.put(50, 'L');
+		hashMap.put(100, 'C');
+		hashMap.put(500, 'D');
+		hashMap.put(1000, 'M');
+
+		int numSize = (int) (Math.log10(num) + 1);
+		String result = "";
+		
+		for (int i = 0; i < numSize; i++) {
+			int num10 = (int) Math.pow(10, numSize - 1 - i);
+			int digitNum = num / num10;
+
+			if (digitNum == 4) {
+				result += hashMap.get(num10);
+				result += hashMap.get(num10 * 5);
+
+			} else if (digitNum == 9) {
+				result += hashMap.get(num10);
+				result += hashMap.get(num10 * 10);
+
+			} else {
+				if (digitNum > 4) {
+					result += hashMap.get(num10 * 5);
+				}
+				for (int j = 0; j < digitNum%5; j++) {
+					result += hashMap.get(num10);
+				}
+
+			}
+
+			num -=digitNum*num10;
+		}
+
+		return result;
+	}
+
+	public static int romanToInt(String s) {
+		/*
+		 * hashmap에 각 문자가 의미하는값을 넣어놓고 입력받은 값을 뒤에서부터 차례대로 읽어가며 더한다 뒤에숫자가 더큰경우
+		 * 다음값을 더하지않고 뺴주면된다.
+		 */
+
+		HashMap<Character, Integer> hashMap = new HashMap<>();
+		hashMap.put('I', 1);
+		hashMap.put('V', 5);
+		hashMap.put('X', 10);
+		hashMap.put('L', 50);
+		hashMap.put('C', 100);
+		hashMap.put('D', 500);
+		hashMap.put('M', 1000);
+
+		int sumNum = 0;
+		sumNum = hashMap.get(s.charAt(s.length() - 1));
+		for (int i = 0; i < s.length() - 1; i++) {
+			if (hashMap.get(s.charAt(s.length() - 1 - i)) > hashMap.get(s.charAt(s.length() - 1 - i - 1))) {
+				sumNum -= hashMap.get(s.charAt(s.length() - 1 - i - 1));
+			} else {
+				sumNum += hashMap.get(s.charAt(s.length() - 1 - i - 1));
+			}
+
+		}
+
+		return sumNum;
+	}
+}
