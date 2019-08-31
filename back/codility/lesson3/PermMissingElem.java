207b7b8276270dbd2c21f3c705a98045f86507c8
seungin yang <rhfpdk92@naver.com>
2019-08-31 23:42:52 +0900

    PermMissingElem

---
+++ back/codility/lesson3/PermMissingElem.java
@@ -0,0 +1,18 @@
+package lesson3;
+
+public class PermMissingElem {
+	public static void main(String[] args) {
+		System.out.println(solution(new int[] { 2, 3, 1, 5 }));
+
+	}
+
+	public static int solution(int[] A) {
+		double sum = 0;
+
+		for (int num : A) {
+			sum += num;
+		}
+
+		return (int)((double)(A.length + 1) * (A.length + 2) / 2 - sum);
+	}
+}
