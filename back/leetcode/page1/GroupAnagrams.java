65207e4b9d16729d7234663acb6d4979c5a68590
seungin yang <rhfpdk92@naver.com>
2019-09-03 09:29:23 +0900

    GroupAnagrama

--- back/leetcode/page1/GroupAnagrams.java
+++ back/leetcode/page1/GroupAnagrams.java
@@ -14,22 +14,31 @@ public class GroupAnagrams {
 
 	public static List<List<String>> groupAnagrams(String[] strs) {
 		List<List<String>> result = new ArrayList<List<String>>();
-		Map<String,String> map = new HashMap<>();
+		Map<String,List<String>> map = new HashMap<>();
 		
 		
 		for(String str:strs){
 			char[] tmp = str.toCharArray();
 			Arrays.sort(tmp);
 			String sortedStr=String.valueOf(tmp);
-			map.put(str, sortedStr);
-		}
-		List<String> subList = new ArrayList<String>();
-		for(int i=0;i<strs.length;i++){
-			for(int j=i+1;j<strs.length;j++){
-				if(map.get(strs[i])==map.get(strs[j]))
-					subList.add(map.get(strs[i]));// 이게아님
+			
+			if(map.containsKey(sortedStr)){
+				map.get(sortedStr).add(str);
+			}else{
+				List<String> addList = new ArrayList<>();
+				addList.add(str);
+				map.put(sortedStr, addList);
 			}
 		}
+
+		for(String key:map.keySet()){
+			List<String> printList =map.get(key);
+			if(printList.size()>0){
+				result.add(printList);
+			}
+			
+		}
+		
 		
 		
 
