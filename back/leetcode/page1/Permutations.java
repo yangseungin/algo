b57fb58cc84ef66b55e5fb95de14f387e11aa971
seungin yang <rhfpdk92@naver.com>
2019-09-07 16:17:12 +0900

    Permutations

--- back/leetcode/page1/Permutations.java
+++ back/leetcode/page1/Permutations.java
@@ -1,31 +1,47 @@
 package page1;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Permutations {
 	public static void main(String[] args) {
-		System.out.println(permute(new int[]{1,2,3}));
+		System.out.println(permute(new int[] { 1, 2, 3 }));
 	}
 
 	public static List<List<Integer>> permute(int[] nums) {
-		List<List<Integer>> result=new ArrayList<List<Integer>>();
-		
-		
+		List<List<Integer>> result = new ArrayList<List<Integer>>();
+
+		if (nums == null || nums.length == 0) {
+			return result;
+		}
+		Arrays.sort(nums);
+
 		List<Integer> combination = new ArrayList<>();
-		
-		combine(result,combination,nums,0);
-		
+
+		combine(result, combination, nums);
+
 		return result;
-		
+
 	}
-	
-	public static void combine(List<List<Integer>> result,List<Integer> combination,int[] nums,int index){
-		
-		
-		
+
+	public static void combine(List<List<Integer>> result, List<Integer> combination, int[] nums) {
+		if (nums.length == combination.size()) {
+			result.add(new ArrayList<>(combination));
+			return ;
+		}
+
+		for (int i = 0; i < nums.length; i++) {
+			if (combination.contains(nums[i])) {
+				continue;
+			}
+			System.out.println("인덱스: " + i + " / " + nums[i] + " / " + combination.toString());
+			combination.add(nums[i]);
+			combine(result, combination, nums);
+			combination.remove(combination.size() - 1);
+
+		}
+
 	}
-	
-	
 
 }
