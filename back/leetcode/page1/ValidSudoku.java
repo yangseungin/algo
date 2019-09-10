2958379b917c4bc08866eccbc5161a720da0e13b
seungin yang <rhfpdk92@naver.com>
2019-09-11 08:58:02 +0900

    valid-sudoku

--- back/leetcode/page1/ValidSudoku.java
+++ back/leetcode/page1/ValidSudoku.java
@@ -7,6 +7,11 @@ public class ValidSudoku {
 				{ '8', '.', '.', '.', '6', '.', '.', '.', '3' }, { '4', '.', '.', '8', '.', '3', '.', '.', '1' },
 				{ '7', '.', '.', '.', '2', '.', '.', '.', '6' }, { '.', '6', '.', '.', '.', '.', '2', '8', '.' },
 				{ '.', '.', '.', '4', '1', '9', '.', '.', '5' }, { '.', '.', '.', '.', '8', '.', '.', '7', '9' } }));
+		System.out.println(isValidSudoku(new char[][] { { '.', '.', '.', '.', '5', '.', '.', '1', '.' },
+				{ '.', '4', '.', '3', '.', '.', '.', '.', '.' }, { '.', '.', '.', '.', '.', '3', '.', '.', '1' },
+				{ '8', '.', '.', '.', '.', '.', '.', '2', '.' }, { '.', '.', '2', '.', '7', '.', '.', '.', '.' },
+				{ '.', '1', '5', '.', '.', '.', '.', '.', '.' }, { '.', '.', '.', '.', '.', '2', '.', '.', '.' },
+				{ '.', '2', '.', '9', '.', '.', '.', '.', '.' }, { '.', '.', '4', '.', '.', '.', '.', '.', '.' } }));
 
 	}
 
@@ -17,39 +22,44 @@ public class ValidSudoku {
 		for (int i = 0; i < 9; i++) {
 			boolean[] sub = new boolean[9];
 			for (int j = 0; j < 9; j++) {
-				if(board[i][j]!='.'){
-					if(sub[board[i][j]-'1']){
+				if (board[i][j] != '.') {
+					if (sub[board[i][j] - '1']) {
 						return false;
 					}
-					sub[board[i][j]-'1']=true;
+					sub[board[i][j] - '1'] = true;
 				}
 			}
 		}
-		
-		
-		for(int i=0;i<9;i++){
+
+		for (int i = 0; i < 9; i++) {
 			boolean[] sub = new boolean[9];
-			for(int j=0;j<9;j++){
-				if(board[j][i]!='.'){
-					if(sub[board[j][i]-'1']){
+			for (int j = 0; j < 9; j++) {
+				if (board[j][i] != '.') {
+					if (sub[board[j][i] - '1']) {
 						return false;
 					}
-					sub[board[j][i]-'1']=true;
+					sub[board[j][i] - '1'] = true;
 				}
-				
+
 			}
 		}
-		
-		//9칸단위확인
-		for(int b=0;b<9;b++){
+
+		// 9칸단위확인
+		for (int b = 0; b < 9; b++) {
 			boolean[] sub = new boolean[9];
-			
-			
-			
+			for (int i = b / 3 * 3; i < b / 3 * 3 + 3; i++) {
+				for (int j = b % 3 * 3; j < b % 3 * 3 + 3; j++) {
+					if (board[i][j] != '.') {
+						if (sub[board[i][j] - '1']) {
+							return false;
+						}
+						sub[board[i][j] - '1'] = true;
+					}
+
+				}
+			}
+
 		}
-		
-		
-		
 
 		return true;
 
