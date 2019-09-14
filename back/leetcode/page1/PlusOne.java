814d053bda17a7fb0cbd396fd5c69de8c9ebb2ef
seungin yang <rhfpdk92@naver.com>
2019-09-14 16:36:35 +0900

    PlusOne

---
+++ back/leetcode/page1/PlusOne.java
@@ -0,0 +1,39 @@
+package page1;
+
+import java.util.Arrays;
+
+public class PlusOne {
+	public static void main(String[] args) {
+		System.out.println(plusOne(new int[]{9,9,9,9,9,9,9}));
+	}
+
+	public static int[] plusOne(int[] digits) {
+		int[] result = new int[digits.length+1];
+		int len = digits.length;
+		
+		digits[len-1]=digits[len-1]+1;
+		if(digits[len-1]<10){
+			return digits;
+		}
+		
+		for(int i=digits.length-1;i>0;i--){
+			if(digits[i]>9){
+				carry(i,digits);
+			}
+		}
+		if(digits[0]<10){
+			return digits;
+		}
+		
+		result[0]=1;
+
+		return result;
+	}
+
+	public static void carry(int index, int[] digits) {
+		digits[index] = 0;
+		digits[index - 1] = digits[index - 1] + 1;
+		
+	}
+
+}
