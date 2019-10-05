978684232bf010cdbc3595106a2098a888a83190
seungin yang <rhfpdk92@naver.com>
2019-10-05 21:01:40 +0900

    best-time-to-buy-and-sell-stock-2

---
+++ back/leetcode/page2/BestTimetoBuyandSellStock2.java
@@ -0,0 +1,23 @@
+package page2;
+
+public class BestTimetoBuyandSellStock2 {
+	public static void main(String[] args) {
+		System.out.println(maxProfit(new int[]{7,1,5,3,6,4}));
+	}
+
+	public static int maxProfit(int[] prices) {
+		if(prices==null||prices.length==0){
+			return 0;
+		}
+		int result=0;
+		for(int i=0;i<prices.length-1;i++){
+			if(prices[i+1]>prices[i]){
+				result+=prices[i+1]-prices[i];
+			}
+			
+		}		
+		
+		return result;
+	}
+
+}
