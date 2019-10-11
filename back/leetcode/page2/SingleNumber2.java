68cd081021b372086e5513757bdb3c4fa13f5a6f
seungin yang <rhfpdk92@naver.com>
2019-10-11 09:17:22 +0900

    single-number2

---
+++ back/leetcode/page2/SingleNumber2.java
@@ -0,0 +1,34 @@
+package page2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SingleNumber2 {
+	public static void main(String[] args) {
+		System.out.println(singleNumber(new int[] { 2, 2, 3, 2 }));
+		System.out.println(singleNumber(new int[] { 0, 1, 0, 1, 0, 1, 99 }));
+
+	}
+
+	public static int singleNumber(int[] nums) {
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		int result = 0;
+		for (int i : nums) {
+			
+			if(map.containsKey(i)){
+				map.put(i, map.get(i) + 1);	
+			}else{
+				map.put(i, 1);
+			}
+		}
+
+		for (int key : map.keySet()) {
+			if(map.get(key)==1){
+				result=key;
+			}
+		}
+
+		return result;
+	}
+
+}
