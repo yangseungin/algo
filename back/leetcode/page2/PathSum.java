d5e95bd5d706468c874582183fec76a59d8457fd
seungin yang <rhfpdk92@naver.com>
2019-10-13 22:10:09 +0900

    pathsum

---
+++ back/leetcode/page2/PathSum.java
@@ -0,0 +1,22 @@
+package page2;
+
+public class PathSum {
+	public static void main(String[] args) {
+		System.out.println();
+	}
+
+	public static boolean hasPathSum(TreeNode root, int sum) {
+
+		return true;
+	}
+
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+}
