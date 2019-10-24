5ea1d9c9571209bc68ca9eff4eef39116e880616
seungin yang <rhfpdk92@naver.com>
2019-10-25 08:33:02 +0900

    find-peak-element

---
+++ back/leetcode/page2/FindPeakElement.java
@@ -0,0 +1,18 @@
+package page2;
+
+public class FindPeakElement {
+	public static void main(String[] args) {
+		System.out.println(findPeakElement(new int[] { 1, 2, 3, 1 }));
+	}
+
+	public static int findPeakElement(int[] nums) {
+		for (int i = 0; i < nums.length-1; i++) {
+			if(nums[i]>nums[i+1]){
+				return i;
+			}
+		}
+
+		return nums.length-1;
+	}
+
+}
