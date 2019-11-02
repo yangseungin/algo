16a4ef0b94877c6beadef946043a49930b0d5ce8
seungin yang <rhfpdk92@naver.com>
2019-11-02 14:31:53 +0900

    power-of-two

---
+++ back/leetcode/page3/PowerofTwo.java
@@ -0,0 +1,21 @@
+package page3;
+
+public class PowerofTwo {
+	public static void main(String[] args) {
+
+		System.out.println(isPowerOfTwo(16));
+
+	}
+
+	public static boolean isPowerOfTwo(int n) {
+		if (n <= 0) {
+			return false;
+		}
+		while (n % 2 == 0) {
+			n /= 2;
+		}
+		return n == 1;
+
+	}
+
+}
