c81dc8f2d08e873ce5609770552da0cb61bec2ed
seungin yang <rhfpdk92@naver.com>
2019-11-23 19:53:55 +0900

    reverseString

---
+++ back/leetcode/page4/ReverseString.java
@@ -0,0 +1,22 @@
+package page4;
+
+import java.util.Arrays;
+
+public class ReverseString {
+	public static void main(String[] args) {
+
+		reverseString(new char[] { 'h', 'e', 'l', 'l', 'o' });
+
+	}
+
+	public static void reverseString(char[] s) {
+		for (int i = 0, j = s.length - 1; i < s.length / 2; i++, j--) {
+			char temp = s[j];
+			s[j] = s[i];
+			s[i] = temp;
+		}
+
+		System.out.println(Arrays.toString(s));
+	}
+
+}
