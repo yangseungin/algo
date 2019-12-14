e1dd575cf562bdc6d446326d832b018187cb1593
seungin yang <rhfpdk92@naver.com>
2019-12-14 22:08:33 +0900

    hamming distance

---
+++ back/leetcode/page5/HammingDistance.java
@@ -0,0 +1,20 @@
+package page5;
+
+public class HammingDistance {
+	public static void main(String[] args) {
+		System.out.println(hammingDistance(1, 4));
+	}
+
+	public static int hammingDistance(int x, int y) {
+		int diff = 0;
+		while (x > 0 || y > 0) {
+			if ((x & 1) != (y & 1)) {
+				diff++;
+			}
+			y = y >> 1;
+			x = x >> 1;
+		}
+		return diff;
+	}
+
+}
