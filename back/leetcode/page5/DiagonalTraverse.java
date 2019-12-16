056d1d60b5a664616738afcf7769a6e868eae748
seungin yang <rhfpdk92@naver.com>
2019-12-16 09:01:01 +0900

    diagonal traverse

---
+++ back/leetcode/page5/DiagonalTraverse.java
@@ -0,0 +1,41 @@
+package page5;
+
+import java.util.Arrays;
+
+public class DiagonalTraverse {
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(findDiagonalOrder(new int[][] { { 1, 2, 3, }, { 4, 5, 6 }, { 7, 8, 9 } })));
+	}
+
+	public static int[] findDiagonalOrder(int[][] matrix) {
+		int row = 0, col = 0;
+		int m = matrix.length;
+		int n = matrix[0].length;
+		int[] result = new int[m * n];
+
+		for (int i = 0; i < result.length; i++) {
+			result[i] = matrix[row][col];
+
+			if ((row + col) % 2 == 0) { // 대각선 라
+				if (col == n - 1) {
+					row++;
+				} else if (row == 0) {
+					col++;
+				} else {
+					row--;
+					col++;
+				}
+			} else {
+				if (row == m - 1) {
+					col++;
+				} else if (col == 0) {
+					row++;
+				} else {
+					row++;
+					col--;
+				}
+			}
+		}
+		return result;
+	}
+}
