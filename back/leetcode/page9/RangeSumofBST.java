7edb45c9eb4184761260eaa75a4d87b0c7c49f47
seungin yang <rhfpdk92@naver.com>
2020-03-27 22:13:22 +0900

    range sum of bst

---
+++ back/leetcode/page9/RangeSumofBST.java
@@ -0,0 +1,33 @@
+package page9;
+
+public class RangeSumofBST {
+	int ans;
+
+	public int rangeSumBST(TreeNode root, int L, int R) {
+		ans = 0;
+		dfs(root, L, R);
+		return ans;
+	}
+
+	public void dfs(TreeNode node, int L, int R) {
+		if (node != null) {
+			if (L <= node.val && node.val <= R)
+				ans += node.val;
+			if (L < node.val)
+				dfs(node.left, L, R);
+			if (node.val < R)
+				dfs(node.right, L, R);
+		}
+	}
+
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+
+}
