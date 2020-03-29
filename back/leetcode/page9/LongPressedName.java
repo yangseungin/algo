59c42d422f018fb19229e6b9abf1e5f8e91539d7
seungin yang <rhfpdk92@naver.com>
2020-03-29 23:26:59 +0900

    logn pressed name

---
+++ back/leetcode/page9/LongPressedName.java
@@ -0,0 +1,14 @@
+package page9;
+
+public class LongPressedName {
+	public static void main(String[] args) {
+		System.out.println(isLongPressedName("alex", "aaleex"));
+	}
+
+	public static boolean isLongPressedName(String name, String typed) {
+		
+
+		return true;
+	}
+
+}
