37c0034493c283a807b7f3611db4072d3b32919b
seungin yang <rhfpdk92@naver.com>
2020-04-03 17:50:29 +0900

    week 1 clear

--- back/leetcode/day30challenge/HappyNumber.java
+++ back/leetcode/day30challenge/HappyNumber.java
@@ -1,5 +1,32 @@
 package day30challenge;
 
-public class HappyNumber {
+import java.util.ArrayList;
+import java.util.List;
 
+public class HappyNumber {
+	 public boolean isHappy(int n) {
+	        List<Integer> list = new ArrayList<>();
+			list.add(n);
+			while (getNumber(n) != 1) {
+				n = getNumber(n);
+
+				if (list.contains(n)) {
+					return false;
+				}
+				
+				list.add(n);
+			}
+			
+			
+			return true;
+	    }
+	    public static int getNumber(int n){
+			String strNum=Integer.toString(n);
+			int tmp=0;
+			for(int i=0;i<strNum.length();i++){
+				tmp+=Math.pow(strNum.charAt(i)-'0',2);
+			}
+			
+			return tmp;
+		}
 }
