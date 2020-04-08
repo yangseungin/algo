62c0f1e8e826ff9fd0ce8a2ec9d384b9c0322e60
seungin yang <rhfpdk92@naver.com>
2020-04-08 21:33:13 +0900

    group anagrams

--- back/leetcode/day30challenge/GroupAnagrams.java
+++ back/leetcode/day30challenge/GroupAnagrams.java
@@ -1,37 +1,38 @@
 package day30challenge;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class GroupAnagrams {
 	public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> result = new ArrayList<List<String>>();
-		Map<String,List<String>> map = new HashMap<>();
-		
-		
-		for(String str:strs){
+		List<List<String>> result = new ArrayList<List<String>>();
+		Map<String, List<String>> map = new HashMap<>();
+
+		for (String str : strs) {
 			char[] tmp = str.toCharArray();
 			Arrays.sort(tmp);
-			String sortedStr=String.valueOf(tmp);
-			
-			if(map.containsKey(sortedStr)){
+			String sortedStr = String.valueOf(tmp);
+
+			if (map.containsKey(sortedStr)) {
 				map.get(sortedStr).add(str);
-			}else{
+			} else {
 				List<String> addList = new ArrayList<>();
 				addList.add(str);
 				map.put(sortedStr, addList);
 			}
 		}
 
-		for(String key:map.keySet()){
-			List<String> printList =map.get(key);
-			if(printList.size()>0){
-			
-               result.add(printList);
-           }
+		for (String key : map.keySet()) {
+			List<String> printList = map.get(key);
+			if (printList.size() > 0) {
+				result.add(printList);
+			}
 		}
-		
-		
-		
 
 		return result;
-   }
+	}
 
 }
