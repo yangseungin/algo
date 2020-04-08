4233e8549285d0155da93fd2463631324fe68ee8
seungin yang <rhfpdk92@naver.com>
2020-04-08 21:35:40 +0900

    group anagrams

--- back/leetcode/day30challenge/CountingElements.java
+++ back/leetcode/day30challenge/CountingElements.java
@@ -5,24 +5,24 @@ import java.util.Set;
 
 public class CountingElements {
 	public static void main(String[] args) {
-		System.out.println(countElements(new int[]{1,2,3}));
-		System.out.println(countElements(new int[]{1,1,3,3,5,5,7,7}));
+		System.out.println(countElements(new int[] { 1, 2, 3 }));
+		System.out.println(countElements(new int[] { 1, 1, 3, 3, 5, 5, 7, 7 }));
 	}
 
 	public static int countElements(int[] arr) {
 		Set<Integer> set = new HashSet<>();
-		int count=0;
-		
-		for(int i:arr){
+		int count = 0;
+
+		for (int i : arr) {
 			set.add(i);
 		}
-		
-		for(int i:arr){
-			if(set.contains(i+1)){
+
+		for (int i : arr) {
+			if (set.contains(i + 1)) {
 				count++;
 			}
 		}
-		
+
 		return count;
 	}
 
