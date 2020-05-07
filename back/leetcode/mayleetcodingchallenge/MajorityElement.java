68aea0eb9a5242c726f8bbb7f56ef5284436c193
seungin yang <rhfpdk92@naver.com>
2020-05-07 13:46:45 +0900

    majority element

---
+++ back/leetcode/mayleetcodingchallenge/MajorityElement.java
@@ -0,0 +1,36 @@
+package mayleetcodingchallenge;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MajorityElement {
+	public static void main(String[] args) {
+		System.out.println(majorityElement(new int[] { 3, 2, 3 }));
+	}
+
+	public static int majorityElement(int[] nums) {
+		Map<Integer, Integer> counts = countNums(nums);
+
+		Map.Entry<Integer, Integer> majorityEntry = null;
+		for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+			if (majorityEntry == null || entry.getValue() > majorityEntry.getValue()) {
+				majorityEntry = entry;
+			}
+		}
+
+		return majorityEntry.getKey();
+	}
+
+	public static Map<Integer, Integer> countNums(int[] nums) {
+		Map<Integer, Integer> counts = new HashMap<Integer, Integer>();
+		for (int num : nums) {
+			if (!counts.containsKey(num)) {
+				counts.put(num, 1);
+			} else {
+				counts.put(num, counts.get(num) + 1);
+			}
+		}
+		return counts;
+	}
+
+}
