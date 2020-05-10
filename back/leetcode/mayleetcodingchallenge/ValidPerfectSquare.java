36e34aed3bcd307bce4a536b6eb3c160c5f608df
seungin yang <rhfpdk92@naver.com>
2020-05-10 22:52:26 +0900

    valid perfect square

---
+++ back/leetcode/mayleetcodingchallenge/ValidPerfectSquare.java
@@ -0,0 +1,18 @@
+package mayleetcodingchallenge;
+
+public class ValidPerfectSquare {
+	public boolean isPerfectSquare(int num) {
+        if (num == 1) {
+			return true;
+		}
+
+		for (int i = 1; i <= num / 2; i++) {
+			if (i * i == num) {
+				return true;
+			}
+		}
+
+		return false;
+    }
+
+}
