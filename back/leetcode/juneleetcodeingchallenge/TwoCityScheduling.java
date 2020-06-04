edc5825743600ae712bed224497ce04209d39067
seungin yang <rhfpdk92@naver.com>
2020-06-04 14:49:44 +0900

    two city scheduling

--- back/leetcode/juneleetcodeingchallenge/TwoCityScheduling.java
+++ back/leetcode/juneleetcodeingchallenge/TwoCityScheduling.java
@@ -8,19 +8,14 @@ public class TwoCityScheduling {
 	}
 
 	public static int twoCitySchedCost(int[][] costs) {
-		int N = costs.length / 2;
 		int[] refund = new int[costs.length];
 		int minCost = 0, index = 0;
-		System.out.println(Arrays.toString(refund));
 		for (int[] cost : costs) {
 			refund[index++] = cost[1] - cost[0];
 			minCost += cost[0];
 		}
-		System.out.println(Arrays.toString(refund));
-		System.out.println(minCost);
 		Arrays.sort(refund);
-		System.out.println(Arrays.toString(refund));
-		for (int i = 0; i < costs.length/2; i++) {
+		for (int i = 0; i < costs.length / 2; i++) {
 			minCost += refund[i];
 		}
 		return minCost;
