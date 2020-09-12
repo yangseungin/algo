c3fd5656e9fe6f7f9ba0afe3762b800713d0a9e0
seungin yang <rhfpdk92@naver.com>
2020-09-12 23:36:07 +0900

    알람 시계

--- back/src/back/Back2884.java
+++ back/src/back/Back2884.java
@@ -9,14 +9,14 @@ public class Back2884 {
 		int hh=sc.nextInt();
 		int mm=sc.nextInt();
 		
-		if(mm>45)
+		if(mm>=45)
 			mm-=45;
 		else{
 			if(hh-1<0)
 				hh=23;
 			else
 				hh-=1;
-			mm=60+mm-45;
+			mm+=15;
 			
 		}
 		System.out.println(hh +" "+mm);
