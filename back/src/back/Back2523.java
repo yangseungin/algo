7a5421207beb7753731589bec439edb5d47cb009
seungin yang <rhfpdk92@naver.com>
2020-09-13 00:16:49 +0900

    별찍기 -13

---
+++ back/src/back/Back2523.java
@@ -0,0 +1,22 @@
+package back;
+
+        import java.util.Scanner;
+
+public class Back2523 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n;i>0;i--){
+            for(int j=i-1;j>0;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
