220a599b816168d00733ec4469d6df91b638938b
seungin yang <rhfpdk92@naver.com>
2020-09-20 20:36:28 +0900

    length of last word

---
+++ back/leetcode/September/LengthofLastWord.java
@@ -0,0 +1,21 @@
+package September;
+
+public class LengthofLastWord {
+    public static void main(String[] args) {
+        System.out.println(lengthOfLastWord("Hello World"));
+    }
+
+    public static int lengthOfLastWord(String s) {
+        s = s.trim();
+        int result = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                result++;
+            } else {
+                break;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ back/leetcode/September/UniquePathsIII.java
@@ -0,0 +1,4 @@
+package September;
+
+public class UniquePathsIII {
+}
