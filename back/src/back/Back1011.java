5aaf06828dced4af0e80c910339dba0a4075267a
seungin yang <rhfpdk92@naver.com>
2020-09-25 23:35:56 +0900

    1011 Fly me to the alpha centauri

--- back/src/back/Back1011.java
+++ back/src/back/Back1011.java
@@ -4,36 +4,23 @@ import java.util.Scanner;
 
 public class Back1011 {
 	public static void main(String[] args) {
-
 		Scanner sc = new Scanner(System.in);
 		int input = sc.nextInt();
 
 		for (int i = 0; i < input; i++) {
-			int minN, powN, maxN, warpCount = 0;
-			int num = 0;
-
 			int numX = sc.nextInt();
 			int numY = sc.nextInt();
 			int cha = numY - numX;
 
-			num++;
-			while (true) {
-				powN = num * num;
-				minN = powN = num + 1;
-				maxN = powN + 1 + num - 1;
-
-				if (minN <= cha && cha <= maxN) {
-					if (minN <= cha && cha <= powN)
-						warpCount = (num << 1) - 1;
-					else
-						warpCount = num << 1;
-					break;
-				}
-
-				num++;
+			int max = (int)Math.sqrt(cha);
+			if(max == Math.sqrt(cha)){
+				System.out.println(max * 2 -1);
+			}else if(cha <= max * max + max){
+				System.out.println(max *2);
+			}else{
+				System.out.println(max *2 +1);
 			}
 
-			System.out.println(warpCount);
 		}
 
 	}
