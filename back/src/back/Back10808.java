c7675ecb93c407d419185aa2be75b2dd83ff2bb4
seungin yang <rhfpdk92@naver.com>
2020-09-27 19:57:32 +0900

    10808 알파벳 개수

---
+++ back/src/back/Back10808.java
@@ -0,0 +1,27 @@
+package back;
+
+        import java.io.BufferedReader;
+        import java.io.IOException;
+        import java.io.InputStreamReader;
+
+public class Back10808 {
+    public static void main(String[] args) throws IOException {
+        int[] alpha = new int[26];
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine();
+
+        for (char c : word.toCharArray()) {
+            alpha[c - 'a']++;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < alpha.length; i++) {
+            sb.append(alpha[i]);
+            if (i != 25)
+                sb.append(" ");
+        }
+
+        System.out.println(sb.toString());
+
+    }
+}
