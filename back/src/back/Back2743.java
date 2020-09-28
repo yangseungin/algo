6c366ac0fa50e4d6a5060661beb1d4aca76f2d6e
seungin yang <rhfpdk92@naver.com>
2020-09-28 23:22:35 +0900

    2743 단어 길이재기

---
+++ back/src/back/Back2743.java
@@ -0,0 +1,10 @@
+package back;
+
+import java.util.Scanner;
+
+public class Back2743 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextLine().length());
+    }
+}
