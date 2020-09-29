165286dbd80ff834e1f6ce1fe59eac3e67a3584a
seungin yang <rhfpdk92@naver.com>
2020-09-29 15:58:56 +0900

    5337 웰컴

---
+++ back/src/back/Back5337.java
@@ -0,0 +1,7 @@
+package back;
+
+public class Back5337 {
+    public static void main(String[] args) {
+        System.out.println(".  .   .\n" + "|  | _ | _. _ ._ _  _\n" + "|/\\|(/.|(_.(_)[ | )(/.\n");
+    }
+}
