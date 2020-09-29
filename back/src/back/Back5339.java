ae7a15e3a8cb813620297c520394b4b492d05dd1
seungin yang <rhfpdk92@naver.com>
2020-09-29 16:02:05 +0900

    5339 콜센터

---
+++ back/src/back/Back5339.java
@@ -0,0 +1,7 @@
+package back;
+
+public class Back5339 {
+    public static void main(String[] args) {
+        System.out.println("     /~\\\n    ( oo|\n    _\\=/_\n   /  _  \\\n  //|/.\\|\\\\\n ||  \\ /  ||\n============\n|          |\n|          |\n|          |");
+    }
+}
