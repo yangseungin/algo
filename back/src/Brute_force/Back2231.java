1850a878d8f1cdd2b0f74f21ca8a484755632389
seungin yang <rhfpdk92@naver.com>
2020-10-14 20:46:25 +0900

    2231 분해합

--- back/src/Brute_force/Back2231.java
+++ back/src/Brute_force/Back2231.java
@@ -1,2 +1,27 @@
-package Brute_force;public class Back2231 {
+package Brute_force;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Back2231 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int idx=N;
+        int res=0;
+
+        while (idx-->0){
+            int tmp=idx;
+            int cal=idx;
+            while(cal!=0){
+                tmp +=cal%10;
+                cal/=10;
+            }
+            if(tmp==N)
+                res=idx;
+        }
+        System.out.println(res);
+
+    }
 }
