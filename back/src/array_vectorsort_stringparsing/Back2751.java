b0950858c5ec03526b40ee09f97dedf2d0eb861e
seungin yang <rhfpdk92@naver.com>
2020-10-18 20:07:17 +0900

    2751 수 정렬하기2

---
+++ back/src/array_vectorsort_stringparsing/Back2751.java
@@ -0,0 +1,25 @@
+package array_vectorsort_stringparsing;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Back2751 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        List<Integer> list = new ArrayList<>();
+        for(int i=0;i<N;i++)
+            list.add(Integer.parseInt(br.readLine()));
+
+        list.sort(Comparator.naturalOrder());
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<N;i++)
+            sb.append(list.get(i)+"\n");
+
+        System.out.println(sb);
+    }
+}
