c39a5211b33cc64e8b6b67b00ab91761b443b1fb
seungin yang <rhfpdk92@naver.com>
2020-10-18 20:35:20 +0900

    10886 덱

--- back/src/deque/Back10866.java
+++ back/src/deque/Back10866.java
@@ -1,58 +1,59 @@
 package deque;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Back10866 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		ArrayList<Integer> list = new ArrayList<Integer>();
-		int N = sc.nextInt();
-		String line = "";
-		for (int i = 0; i <= N; i++) {
-			line = sc.nextLine();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        int N = Integer.parseInt(br.readLine());
+        String line = "";
+        StringBuilder sb =new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            line = br.readLine();
 
-			if (line.contains("push_back")) {
-				list.add(list.size(), Integer.parseInt(line.substring(10, line.length())));
-			} else if (line.contains("push_front")) {
-				list.add(0, Integer.parseInt(line.substring(11, line.length())));
-			} else if (line.equals("pop_front")) {
-				if (list.size() == 0)
-					System.out.println(-1);
-				else {
-					System.out.println(list.get(0));
-					list.remove(0);
-				}
+            if (line.contains("push_back")) {
+                list.add(list.size(), Integer.parseInt(line.substring(10, line.length())));
+            } else if (line.contains("push_front")) {
+                list.add(0, Integer.parseInt(line.substring(11, line.length())));
+            } else if (line.equals("pop_front")) {
+                if (list.size() == 0)
+                    sb.append(-1+"\n");
+                else {
+                    sb.append(list.get(0)+"\n");
+                    list.remove(0);
+                }
 
-			} else if (line.equals("pop_back")) {
-				if (list.size() == 0)
-					System.out.println(-1);
-				else {
-					System.out.println(list.get(list.size() - 1));
-					list.remove(list.size() - 1);
-				}
+            } else if (line.equals("pop_back")) {
+                if (list.size() == 0)
+                    sb.append(-1+"\n");
+                else {
+                    sb.append(list.get(list.size() - 1)+"\n");
+                    list.remove(list.size() - 1);
+                }
 
-			} else if (line.equals("size"))
-				System.out.println(list.size());
-			else if (line.equals("empty")) {
-				if (list.size() == 0)
-					System.out.println(1);
-				else
-					System.out.println(0);
-			} else if (line.equals("front")) {
-				if (list.size() == 0)
-					System.out.println(-1);
-				else
-					System.out.println(list.get(0));
-			} else if (line.equals("back")) {
-				if (list.size() == 0)
-					System.out.println(-1);
-				else
-					System.out.println(list.get(list.size() - 1));
-			}
-
-		}
-
-	}
-
+            } else if (line.equals("size"))
+                sb.append(list.size()+"\n");
+            else if (line.equals("empty")) {
+                if (list.size() == 0)
+                    sb.append(1+"\n");
+                else
+                sb.append(0+"\n");
+            } else if (line.equals("front")) {
+                if (list.size() == 0)
+                    sb.append(-1+"\n");
+                else
+                sb.append(list.get(0)+"\n");
+            } else if (line.equals("back")) {
+                if (list.size() == 0)
+                    sb.append(-1+"\n");
+                else
+                sb.append(list.get(list.size() - 1)+"\n");
+            }
+        }
+        System.out.println(sb);
+    }
 }
