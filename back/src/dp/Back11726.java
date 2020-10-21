612d34b5aa64badf39f5b8f038f8675da99a1177
seungin yang <rhfpdk92@naver.com>
2020-10-21 14:36:08 +0900

    11726 2xn타일링

---
+++ back/src/dp/Back11726.java
@@ -0,0 +1,18 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Back11726 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[1001];
+        arr[1] = 1;
+        arr[2] = 2;
+        for (int i = 3; i <=n; i++)
+            arr[i] = (arr[i - 1] + arr[i - 2]) % 10007;
+        System.out.println(String.valueOf(arr[n]));
+    }
+}
