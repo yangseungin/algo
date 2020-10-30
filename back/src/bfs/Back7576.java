cf1c14ad6bc2f7aa6d113a446f079f78cc1ececc
seungin yang <rhfpdk92@naver.com>
2020-10-30 23:44:28 +0900

    2156 포도주 시식

---
+++ back/src/bfs/Back7576.java
@@ -0,0 +1,2 @@
+package bfs;public class Back7576 {
+}
---
+++ back/src/dp/Back2156.java
@@ -0,0 +1,32 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Back2156 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] dp = new int[N];
+        for (int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(br.readLine());
+
+        if (N >= 1)
+            dp[0] = arr[0];
+
+        if (N >= 2)
+            dp[1] = arr[0] + arr[1];
+
+        if (N >= 3)
+            dp[2] = Math.max(dp[1], Math.max(dp[0] + arr[2], arr[1] + arr[2]));
+
+        for (int i = 3; i < N; i++)
+            dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i]));
+
+        System.out.println(dp[N - 1]);
+
+
+    }
+}
