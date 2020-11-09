1d335b3ef08b4ff8d2ad1e25999e1352b651b91a
seungin yang <rhfpdk92@naver.com>
2020-11-09 12:53:06 +0900

    15688 19944

---
+++ back/src/array_vectorsort_stringparsing/Bacn15688.java
@@ -0,0 +1,21 @@
+package array_vectorsort_stringparsing;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Bacn15688 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(br.readLine());
+        Arrays.sort(arr);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++)
+            sb.append(arr[i]+"\n");
+        System.out.println(sb);
+    }
+}
