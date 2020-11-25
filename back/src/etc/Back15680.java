e875f92c6c3bfdb2341a027d544a74f3c2c80ee2
seungin yang <rhfpdk92@naver.com>
2020-11-25 19:38:24 +0900

    2748 15680

---
+++ back/src/etc/Back15680.java
@@ -0,0 +1,12 @@
+package etc;
+
+import java.util.Scanner;
+
+public class Back15680 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        System.out.println(year - 1946);
+
+    }
+}
