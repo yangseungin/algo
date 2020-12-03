5982608a957bd6e23484acef583909312fb16b41
seungin yang <rhfpdk92@naver.com>
2020-12-03 16:01:44 +0900

    3047 ABC

---
+++ back/src/simulation/Back3047.java
@@ -0,0 +1,24 @@
+package simulation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Back3047 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[3];
+        for (int i = 0; i < 3; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        String input = br.readLine();
+        for (int i = 0; i < input.length(); i++)
+            System.out.print(arr[input.charAt(i) - 'A'] + " ");
+
+
+    }
+}
