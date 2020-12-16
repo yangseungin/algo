562f0ea9bf35de8930e54252ee6b4999abff2a4d
seungin yang <rhfpdk92@naver.com>
2020-12-16 19:45:35 +0900

    15666 N과M(12)

---
+++ back/src/backtracking/Back15666.java
@@ -0,0 +1,48 @@
+package backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Back15666 {
+    static int[] arr, input;
+    static boolean[] visited;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()), M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        input = new int[N];
+        visited = new boolean[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++)
+            input[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(input);
+        sb = new StringBuilder();
+        dfs(N, M, 0,1);
+        System.out.println(sb);
+
+    }
+
+    private static void dfs(int N, int M, int depth, int idx) {
+        if (depth == M) {
+            for (int num : arr)
+                sb.append(num + " ");
+            sb.append("\n");
+            return;
+        }
+        int before = -1;
+        for (int i = idx-1; i < N; i++) {
+            if ( before != input[i]) {
+                arr[depth] = input[i];
+                before = input[i];
+                dfs(N, M, depth + 1,i+1);
+            }
+        }
+    }
+}
