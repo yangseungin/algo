08b45c85476ea330598c16e3e21f3361baa2533b
seungin yang <rhfpdk92@naver.com>
2021-01-12 19:54:13 +0900

    16212 정열적인 정렬

---
+++ back/src/sort/Back16212.java
@@ -0,0 +1,25 @@
+package sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Back16212 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(arr);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arr.length; i++)
+            sb.append(arr[i] + " ");
+
+        System.out.println(sb);
+
+    }
+}
