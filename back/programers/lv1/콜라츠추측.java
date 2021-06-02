d6e25453de59444928bd52d272e459f148bd6e04
seungin yang <rhfpdk92@naver.com>
2021-06-02 23:02:09 +0900

    콜라츠 추측

--- back/programers/lv1/콜라츠추측.java
+++ back/programers/lv1/콜라츠추측.java
@@ -9,18 +9,21 @@ public class 콜라츠추측 {
 
     public static int solution(int num) {
         int answer = 0;
-        while (num != 1) {
-            if (num % 2 == 0) {
-                num /= 2;
-            } else {
-                num = 3 * num + 1;
+
+        while(num != 1){
+            if(num % 2 == 0){
+                num = num/2;
+            } else if(num %2==1){
+                num = (num*3) + 1;
             }
             answer++;
-            if (answer == 500) {
+
+            if(answer == 500) {
                 answer = -1;
                 break;
             }
         }
+
 
         return answer;
     }
