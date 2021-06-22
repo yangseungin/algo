9190322cd4b6af82eb03ece3ae578b1f261cdeb2
seungin yang <rhfpdk92@naver.com>
2021-06-22 22:09:19 +0900

    단어 변환

---
+++ back/programers/lv3/단어변환.java
@@ -0,0 +1,50 @@
+package lv3;
+
+public class 단어변환 {
+    //    https://programmers.co.kr/learn/courses/30/lessons/49189
+    static int answer = 0;
+    static boolean[] visited;
+
+    public static void main(String[] args) {
+        System.out.println(solution("hit", "cog", new String[]{"hot", "dot", "dog", "lot", "log", "cog"}));
+
+    }
+
+    public static int solution(String begin, String target, String[] words) {
+        visited = new boolean[words.length];
+        dfs(begin, target, words, 0);
+        return answer;
+    }
+
+    public static void dfs(String begin, String target, String[] words, int count) {
+        if (begin.equals(target)) {
+            answer = count;
+            return;
+        }
+
+        for (int i = 0; i < words.length; i++) {
+            if (visited[i]) {
+                continue;
+            }
+            if (checkWord(begin, words[i])) {
+                visited[i] = true;
+                dfs(words[i], target, words, count + 1);
+                visited[i] = false;
+            }
+        }
+
+    }
+
+    public static boolean checkWord(String str1, String str2) {
+        int count = 0;
+        for (int i = 0; i < str1.length(); i++) {
+            if (str1.charAt(i) != str2.charAt(i)) {
+                count++;
+            }
+            if (count > 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
