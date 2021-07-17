690a0dbbf107bb44d7ede20a876d057786496574
seungin yang <rhfpdk92@naver.com>
2021-07-17 23:56:56 +0900

    2178 미로탐색

---
+++ back/src/bfs/Back2178.java
@@ -0,0 +1,59 @@
+package bfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Back2178 {
+    static int N;
+    static int M;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            String tmp = br.readLine();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = tmp.charAt(j) - '0';
+            }
+        }
+
+        visited = new boolean[N][M];
+        visited[0][0] = true;
+        bfs(0, 0);
+        System.out.println(map[N - 1][M - 1]);
+    }
+
+    public static void bfs(int x, int y) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{x, y});
+
+        while (!q.isEmpty()) {
+            int[] now = q.poll();
+            int nowX = now[0];
+            int nowY = now[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nextX = nowX + dx[i];
+                int nextY = nowY + dy[i];
+
+                if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= M || visited[nextX][nextY] || map[nextX][nextY] == 0)
+                    continue;
+
+                q.add(new int[]{nextX, nextY});
+                map[nextX][nextY] = map[nowX][nowY] + 1;
+                visited[nextX][nextY] = true;
+            }
+        }
+    }
+}
