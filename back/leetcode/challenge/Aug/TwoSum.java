4e8c12445729a91ced5705a44dbaa54567a55145
seungin yang <rhfpdk92@naver.com>
2021-08-05 08:57:15 +0900

    Twosum

---
+++ back/leetcode/challenge/Aug/TwoSum.java
@@ -0,0 +1,28 @@
+package challenge.Aug;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 9)));
+        System.out.println(Arrays.toString(twoSum(new int[]{3,2,4}, 6)));
+        System.out.println(Arrays.toString(twoSum(new int[]{3,3}, 6)));
+
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i]) && map.get(target - nums[i]) != i) {
+                return new int[] { i, map.get(target - nums[i]) };
+            }
+        }
+
+        return null;
+    }
+}
